55b4acd8c0f0f16512115f8b5def4897d2dbe633
Rudolf Kovalyov <rudolf.kovalyov@gmail.com>
2018-11-05 02:35:03 +0200

    a little refactor

--- packages/zeropack-storybook/.storybook/webpack.config.js
+++ packages/zeropack-storybook/.storybook/webpack.config.js
@@ -8,8 +8,6 @@ const { Arr, Obj } = require('./utils');
 module.exports = (baseConfig, env) => {
   const { webpackConfig: zeropackConfig } = generateContext();
 
-  console.log(baseConfig);
-
   let config = Obj.merge(
     Obj.pick(baseConfig, 'mode', 'devtool', 'entry', 'output', 'performance'),
     Obj.pick(zeropackConfig, 'context'), {
@@ -17,7 +15,9 @@ module.exports = (baseConfig, env) => {
       extensions: Arr.uniqueMerge(baseConfig, zeropackConfig, 'resolve.extensions'),
       modules: Arr.uniqueMerge(baseConfig, zeropackConfig, 'resolve.modules'),
       alias: Obj.deepPropMerge(baseConfig, zeropackConfig, 'resolve.alias')
-    }
+    },
+    module: {},
+    plugins: [],
   });
 
   // webpackConfig.plugins.push(...baseConfig.plugins);  
